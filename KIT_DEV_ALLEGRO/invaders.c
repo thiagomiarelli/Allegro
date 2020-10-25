9be8187ff3d2e7b5f5fc77c7023eabdbc671684f
Thiago Miarelli <46660124+thiagomiarelli@users.noreply.github.com>
2020-10-25 16:55:33 -0300

    update with base

--- KIT_DEV_ALLEGRO/invaders.c
+++ KIT_DEV_ALLEGRO/invaders.c
@@ -4,31 +4,39 @@
 #include <allegro5/allegro_ttf.h>
 #include <allegro5/allegro_primitives.h>
 #include <allegro5/allegro_image.h>
+//#include <stdlib.h>
 
 const float FPS = 60;  
+
 const int SCREEN_W = 1280;
 const int SCREEN_H = 720;
-
-int main(void){
-
-    ALLEGRO_DISPLAY *display = NULL;
+ 
+int main(int argc, char **argv){
+	
+	ALLEGRO_DISPLAY *display = NULL;
 	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
 	ALLEGRO_TIMER *timer = NULL;
+   
+	//----------------------- rotinas de inicializacao ---------------------------------------
     
-    //inicializa o Allegro
+	//inicializa o Allegro
 	if(!al_init()) {
 		fprintf(stderr, "failed to initialize allegro!\n");
 		return -1;
 	}
-
-    //cria uma tela com dimensoes de SCREEN_W, SCREEN_H pixels
-	display = al_create_display(SCREEN_W, SCREEN_H);
-	if(!display) {
-		fprintf(stderr, "failed to create display!\n");
-		al_destroy_timer(timer);
+	
+    //inicializa o módulo de primitivas do Allegro
+    if(!al_init_primitives_addon()){
+		fprintf(stderr, "failed to initialize primitives!\n");
+        return -1;
+    }	
+	
+	//inicializa o modulo que permite carregar imagens no jogo
+	if(!al_init_image_addon()){
+		fprintf(stderr, "failed to initialize image module!\n");
 		return -1;
 	}
-
+   
 	//cria um temporizador que incrementa uma unidade a cada 1.0/FPS segundos
     timer = al_create_timer(1.0 / FPS);
     if(!timer) {
@@ -36,7 +44,42 @@ int main(void){
 		return -1;
 	}
  
-	//cria a fila de eventos
+	//cria uma tela com dimensoes de SCREEN_W, SCREEN_H pixels
+	display = al_create_display(SCREEN_W, SCREEN_H);
+	if(!display) {
+		fprintf(stderr, "failed to create display!\n");
+		al_destroy_timer(timer);
+		return -1;
+	}
+
+	//instala o teclado
+	if(!al_install_keyboard()) {
+		fprintf(stderr, "failed to install keyboard!\n");
+		return -1;
+	}
+	
+	//instala o mouse
+	if(!al_install_mouse()) {
+		fprintf(stderr, "failed to initialize mouse!\n");
+		return -1;
+	}
+
+	//inicializa o modulo allegro que carrega as fontes
+	al_init_font_addon();
+
+	//inicializa o modulo allegro que entende arquivos tff de fontes
+	if(!al_init_ttf_addon()) {
+		fprintf(stderr, "failed to load tff font module!\n");
+		return -1;
+	}
+	
+	//carrega o arquivo arial.ttf da fonte Arial e define que sera usado o tamanho 32 (segundo parametro)
+    ALLEGRO_FONT *size_32 = al_load_font("arial.ttf", 32, 1);   
+	if(size_32 == NULL) {
+		fprintf(stderr, "font file does not exist or cannot be accessed!\n");
+	}
+
+ 	//cria a fila de eventos
 	event_queue = al_create_event_queue();
 	if(!event_queue) {
 		fprintf(stderr, "failed to create event_queue!\n");
@@ -44,6 +87,8 @@ int main(void){
 		al_destroy_timer(timer);
 		return -1;
 	}
+   
+
 
 	//registra na fila os eventos de tela (ex: clicar no X na janela)
 	al_register_event_source(event_queue, al_get_display_event_source(display));
@@ -55,21 +100,49 @@ int main(void){
 	al_register_event_source(event_queue, al_get_mouse_event_source());  	
 
 
-	int playing = 1
-	while (playing)
-	{
-		al_clear_to_color(al_map_rgb(0, 122, 233));
+	//inicia o temporizador
+	al_start_timer(timer);
+	
+	int playing = 1;
+	while(playing) {
+		al_clear_to_color(al_map_rgb(000, 122, 232));
 		al_flip_display();
-
 		ALLEGRO_EVENT ev;
+		//espera por um evento e o armazena na variavel de evento ev
 		al_wait_for_event(event_queue, &ev);
 
-		if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+		//se o tipo de evento for um evento do temporizador, ou seja, se o tempo passou de t para t+1
+		if(ev.type == ALLEGRO_EVENT_TIMER) {
+
+			//atualiza a tela (quando houver algo para mostrar)
+			al_flip_display();
+			
+			if(al_get_timer_count(timer)%(int)FPS == 0)
+				printf("\n%d segundos se passaram...", (int)(al_get_timer_count(timer)/FPS));
+		}
+		//se o tipo de evento for o fechamento da tela (clique no x da janela)
+		else if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
 			playing = 0;
 		}
-	}
-	
+		//se o tipo de evento for um clique de mouse
+		else if(ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
+			printf("\nmouse clicado em: %d, %d", ev.mouse.x, ev.mouse.y);
+		}
+		//se o tipo de evento for um pressionar de uma tecla
+		else if(ev.type == ALLEGRO_EVENT_KEY_DOWN) {
+			//imprime qual tecla foi
+			printf("\ncodigo tecla: %d", ev.keyboard.keycode);
+		}
 
+	} //fim do while
+     
+	//procedimentos de fim de jogo (fecha a tela, limpa a memoria, etc)
+	
+ 
+	al_destroy_timer(timer);
+	al_destroy_display(display);
+	al_destroy_event_queue(event_queue);
+   
+ 
 	return 0;
 }
-
