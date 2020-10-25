5e0af7e776535e83a4798a27d1cc2cbb29fe12d7
Thiago Miarelli <46660124+thiagomiarelli@users.noreply.github.com>
2020-10-25 16:49:24 -0300

    Criação do loop de jogabilidade

--- KIT_DEV_ALLEGRO/invaders.c
+++ KIT_DEV_ALLEGRO/invaders.c
@@ -26,12 +26,50 @@ int main(void){
 	if(!display) {
 		fprintf(stderr, "failed to create display!\n");
 		al_destroy_timer(timer);
-		al_rest(4);
 		return -1;
 	}
 
-	al_clear_to_color(al_map_rgb(021,223,111));
-	al_flip_display();
+	//cria um temporizador que incrementa uma unidade a cada 1.0/FPS segundos
+    timer = al_create_timer(1.0 / FPS);
+    if(!timer) {
+		fprintf(stderr, "failed to create timer!\n");
+		return -1;
+	}
+ 
+	//cria a fila de eventos
+	event_queue = al_create_event_queue();
+	if(!event_queue) {
+		fprintf(stderr, "failed to create event_queue!\n");
+		al_destroy_display(display);
+		al_destroy_timer(timer);
+		return -1;
+	}
+
+	//registra na fila os eventos de tela (ex: clicar no X na janela)
+	al_register_event_source(event_queue, al_get_display_event_source(display));
+	//registra na fila os eventos de tempo: quando o tempo altera de t para t+1
+	al_register_event_source(event_queue, al_get_timer_event_source(timer));
+	//registra na fila os eventos de teclado (ex: pressionar uma tecla)
+	al_register_event_source(event_queue, al_get_keyboard_event_source());
+	//registra na fila os eventos de mouse (ex: clicar em um botao do mouse)
+	al_register_event_source(event_queue, al_get_mouse_event_source());  	
+
+
+	int playing = 1
+	while (playing)
+	{
+		al_clear_to_color(al_map_rgb(0, 122, 233));
+		al_flip_display();
+
+		ALLEGRO_EVENT ev;
+		al_wait_for_event(event_queue, &ev);
+
+		if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+			playing = 0;
+		}
+	}
+	
+
 	return 0;
 }
 
