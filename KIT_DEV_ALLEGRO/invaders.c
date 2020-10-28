3e43f38081b6932923853a495488c2da1c6d92fc
Thiago Miarelli <46660124+thiagomiarelli@users.noreply.github.com>
2020-10-27 23:00:05 -0300

    test

--- KIT_DEV_ALLEGRO/invaders.c
+++ KIT_DEV_ALLEGRO/invaders.c
@@ -265,7 +265,7 @@ void BuildAlienGrid(int linhas, int colunas, ALIEN alien[linhas][colunas], int s
 	int i, j;
 	for(i = 0; i < linhas; i++){
 		for(j = 0; j < colunas; j++){
-			alien[i][j].canto_x = MARGIN_W + (j * (ALIEN_W + DIST_NAVES_W)) + (velocidade * seconds);
+			//alien[i][j].canto_x += MARGIN_W + (j * (ALIEN_W + DIST_NAVES_W)) + (velocidade * seconds);
 			alien[i][j].canto_y = MARGIN_H + (i * (ALIEN_H + DIST_NAVES_H)) + altura;
 			drawAlien(&alien[i][j]);
 
