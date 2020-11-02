19c77c420bb2f778de0aef8e5ab6b2c79e0fa2de
Thiago Miarelli <46660124+thiagomiarelli@users.noreply.github.com>
2020-11-02 20:38:42 -0300

    sound + library

--- KIT_DEV_ALLEGRO/invaders.c
+++ KIT_DEV_ALLEGRO/invaders.c
@@ -4,6 +4,8 @@
 #include <allegro5/allegro_ttf.h>
 #include <allegro5/allegro_primitives.h>
 #include <allegro5/allegro_image.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
 #include <stdlib.h>
 #include <math.h>
 
