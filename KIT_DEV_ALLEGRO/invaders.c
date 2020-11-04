42feb5e0ae3b2ac7398e7c2e438f47a01111c39b
Thiago Miarelli <46660124+thiagomiarelli@users.noreply.github.com>
2020-11-04 10:54:48 -0300

    Update invaders.c

--- KIT_DEV_ALLEGRO/invaders.c
+++ KIT_DEV_ALLEGRO/invaders.c
@@ -957,7 +957,7 @@ void colisaoTiroAlien(TIRO *tiro, NAVE *nave, int *frase_sorteada){
 		tiro -> exist = 0;
 	}
 	
-	if(tiro -> y > FLUTACAO_NAVE && (tiro -> x > (nave -> ponta_x - NAVE_W/2) && tiro -> x < (nave -> ponta_x + NAVE_W/2))){
+	if((tiro -> y > FLUTACAO_NAVE && (tiro -> x > (nave -> ponta_x - NAVE_W/2) && tiro -> x < (nave -> ponta_x + NAVE_W/2))) && tiro ->exist){
 
 		atualizaMoedas(moedas_file, points, 'e');
 				srand(points);
